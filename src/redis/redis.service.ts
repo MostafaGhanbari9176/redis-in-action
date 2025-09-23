767dc3be126bb623b5476378caef817b73c601db
MostafaGhanbari9176 <godhelot1@outlook.com>
2025-09-23 17:49:56 +0330

    feat: limiting otp request

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -12,13 +12,25 @@ export class RedisService {
             await this.client.set(key, value, 'EX', ttl)
         }
         else {
-
             await this.client.set(key, value)
+        }
+    }
+
+    async setNX(key: string, value: string, ttl?: number): Promise<string | null> {
+        if (ttl) {
+            return await this.client.set(key, value, 'EX', ttl, 'NX')
+        }
+        else {
+            return await this.client.set(key, value, 'NX')
         }
     }
 
     async get<T = string>(key: string): Promise<T | null> {
         return await this.client.get(key) as T
+    }
+
+    async exists(key: string): Promise<boolean> {
+        return await this.client.exists(key) === 1
     }
 
 }
