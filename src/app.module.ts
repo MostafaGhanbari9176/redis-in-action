e22ddd20a192885c2fa8128574e51c08c9f80706
MostafaGhanbari9176 <godhelot1@outlook.com>
2025-09-23 20:38:23 +0330

    feat: implementing fixed window rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RedisModule } from './redis/redis.module';
+import { FixedWindowRateLimitGuard } from './common/guard/rate_limitation/fixed-window-limitation.guard';
 
 @Module({
   imports: [DatabaseModule, AuthModule, UserModule, RedisModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: "APP_GUARD",
+      useClass: FixedWindowRateLimitGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -7,6 +7,21 @@ export class RedisService {
         @Inject("REDIS_CLIENT") private readonly client: Redis
     ) { }
 
+    async increment(key: string, ttl?: number): Promise<number> {
+        let isFirst: string | null
+        if (ttl) {
+            isFirst = await this.client.set(key, 1, 'EX', ttl, 'NX')
+        } else {
+            isFirst = await this.client.set(key, 1, 'NX')
+        }
+
+        if (!isFirst) {
+            return await this.client.incr(key)
+        }
+
+        return 1
+    }
+
     async set(key: string, value: string, ttl?: number) {
         if (ttl) {
             await this.client.set(key, value, 'EX', ttl)
