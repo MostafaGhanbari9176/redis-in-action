5760966465e678d33173c01e94c4ec9029572dfc
MostafaGhanbari9176 <godhelot1@outlook.com>
2025-12-08 16:59:01 +0330

    feat(decorator): implement user decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { FixedWindowRateLimitGuard } from './common/guard/rate_limitation/fixed-
 import { SlidingWindowLimitationGuard } from './common/guard/rate_limitation/sliding-window-limitation.guard';
 import { TokenBucketLimitationGuard } from './common/guard/rate_limitation/toke-bucket-limitation.guard';
 import { AuthGuard } from './common/guard/auth.guard';
+import { registerUserDecoratorDb } from './decorator/user.decorator';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
   imports: [DatabaseModule, AuthModule, UserModule, RedisModule],
@@ -25,4 +28,8 @@ import { AuthGuard } from './common/guard/auth.guard';
     }
   ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(@InjectConnection() private readonly connection: Connection) {
+    registerUserDecoratorDb(this.connection);
+  }
+}
